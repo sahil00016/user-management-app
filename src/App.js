db462ebc681fd82ce404b3b170a4e977bc9e8d78
sahil sonker <122673153+sahil0016@users.noreply.github.com>
2025-04-08 16:19:14 +0530

    Initial commit with complete user management app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import UsersPage from './pages/UsersPage';
+import Home from './pages/Home';
+import ProtectedRoute from './components/Shared/ProtectedRoute';
+import Layout from './components/Shared/Layout';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/users" element={
+            <ProtectedRoute>
+              <UsersPage />
+            </ProtectedRoute>
+          } />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
